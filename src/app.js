51acbf490d354890acfd3e045cee9adcbdd39140
Kautilya-23 <kautilyasathwara@gmail.com>
2025-07-09 10:19:08 +0530

    Get APIs

--- src/app.js
+++ src/app.js
@@ -6,6 +6,43 @@ const User = require("./models/user")
 
 app.use(express.json());
 
+// get user by email
+app.get("/user", async(req, res) => {
+    const userEmail = req.body.emailId;
+
+    try{
+        const user = await User.findOne({emailId: userEmail});
+        if(!user) {
+            res.status(400).send("User not found");
+        } else {
+            res.send(user);
+        }
+    }
+
+    // try{
+    //     const users = await User.find({emailId: userEmail});
+    //     if(users.length === 0)
+    //         res.status(400).send("User not found");
+    //     else{
+    //         res.send(users);
+    //     }
+    // }
+    catch (err) {
+        res.status(400).send("Something went wrong!");
+    }
+    
+});
+
+// Feed API - GET /feed - get all the users from database
+app.get("/feed", async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch (err) {
+        res.status(400).send("Something went wrong!");
+    }
+});
+
 app.post("/signup", async (req, res) => {
     // Creating a new instance of a user model
     const user = new User(req.body);
