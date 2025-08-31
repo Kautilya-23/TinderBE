ba84e5e61d64d862457a4efca76128858c27064c
Kautilya-23 <kautilyasathwara@gmail.com>
2025-08-31 14:10:07 +0530

    Chat

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const connectDB = require("./config/database");
 const app = express();
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const http = require("http");
 
 // app.use(cors({
 //   origin: "http://localhost:5173",
@@ -19,7 +20,6 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.options("/profile/edit", cors(corsOptions));
 
-
 app.use(express.json());
 app.use(cookieParser());
 
@@ -27,16 +27,22 @@ const authRouter = require("./routes/auth");
 const profileRouter = require("./routes/profile");
 const requestRouter = require("./routes/request");
 const userRouter = require("./routes/user");
+const initializeSocket = require("./utils/socket");
+const chatRouter = require("./routes/chat");
 
 app.use("/", authRouter);
 app.use("/", profileRouter);
 app.use("/", requestRouter);
 app.use("/", userRouter);
+app.use("/", chatRouter);
+
+const server = http.createServer(app);
+initializeSocket(server);
 
 connectDB()
     .then(() => {
         console.log("Database connection established...");
-        app.listen(7777, () => {
+        server.listen(7777, () => {
         console.log("Server listening the port 7777....");
 });
     })
