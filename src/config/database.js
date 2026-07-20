161ffcc02c5b0878e8b120ddaa433100de0d04c8
Kautilya-23 <kautilyasathwara@gmail.com>
2026-07-20 08:55:29 +0530

    Connection String

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     await mongoose.connect(
-        "mongodb+srv://kautilyasathwara:XQX8Gh6lPVvnLyKM@namastenode.07mzjrb.mongodb.net/TinderBE"
+        "mongodb+srv://kautilyasathwara:XQX8Gh6lPVvnLyKM@namastenode.07mzjrb.mongodb.net/"
     );
 };
 
